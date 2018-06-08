5cf4dab0a1abf73e50741324fe3f1f9a0bcd8838
Dolga4ev <39748092+Dolga4ev@users.noreply.github.com>
2018-06-08 20:04:12 +0300

    Add files via upload

--- src/test/task/Main.java
+++ src/test/task/Main.java
@@ -1,29 +1,19 @@
-package test.task;
+package test.task2;
 
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-
-
+	// write your code here
         Scanner scan = new Scanner(System.in);
         String input;
+        Xml file = new Xml();
+        ArrayList tasks = file.read();
+        Operation op = new Operation();
+
         System.out.println("?help?");
-        Task t = new Task();
-
-
-
         do {
 
             input = scan.nextLine();
@@ -33,52 +23,45 @@ public class Main {
                 System.out.println("list, new, done, in progress, complete i, add, edit i, remove i, exit");
             }
             if(input.equals("list")){
-                t.view();
+                op.view("all");
             }
             if(input.equals("new")){
-                ViewFile.viewList(input);
+                op.view("new");
             }
             if(input.equals("done")){
-                ViewFile.viewList(input);
+                op.view("done");
             }
             if(input.equals("in progress")){
-                ViewFile.viewList("in_progress");
+                op.view("in progress");
             }
+
+            //if(division[0].equals("complete")){
             if(division[0].equals("complete")){
-                t.complete(Integer.parseInt(division[1]));
+                try {
+                    op.complete(Integer.parseInt(division[1]));
+                }
+                catch (Exception ei) {
+                    System.out.println("Вы не ввели номер элемента");
+                }
             }
-            if(division[0].equals("add")){
-                System.out.println("enter the new title");
-                input = scan.nextLine();
-                String title = input;
-                System.out.println("enter the new description");
-                input = scan.nextLine();
-                String description = input;
-                System.out.println("enter the new priority");
-                input = scan.nextLine();
-                String priority = input;
-                System.out.println("enter the new deadline");
-                input = scan.nextLine();
-                String deadline = input;
-                t.add(title, description, priority, deadline);
+            if(input.equals("add")){
+                op.add();
             }
             if(division[0].equals("edit")){
-                System.out.println("enter the title (if you don't want to change, write '-')");
-                input = scan.nextLine();
-                String title = input;
-                System.out.println("enter the description (if you don't want to change, write '-')");
-                input = scan.nextLine();
-                String description = input;
-                System.out.println("enter the priority (if you don't want to change, write '-')");
-                input = scan.nextLine();
-                String priority = input;
-                System.out.println("enter the deadline (if you don't want to change, write '-')");
-                input = scan.nextLine();
-                String deadline = input;
-                t.edit(Integer.parseInt(division[1]), title, description, priority, deadline);
+                try {
+                op.edit(Integer.parseInt(division[1]));
+                }
+                catch (Exception ei) {
+                    System.out.println("Вы не ввели номер элемента");
+                }
             }
             if(division[0].equals("remove")){
-                t.remove(Integer.parseInt(division[1]));
+                try {
+                op.remove(Integer.parseInt(division[1]));
+                }
+                catch (Exception ei) {
+                    System.out.println("Вы не ввели номер элемента");
+                }
             }
             if(input.equals("exit")){
                 System.out.println("see you soon");
@@ -88,3 +71,4 @@ public class Main {
 
     }
 }
+
--- src/test/task/Task.java
+++ src/test/task/Task.java
@@ -1,168 +1,67 @@
-package test.task;
+package test.task2;
 
-import org.w3c.dom.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
+public class Task {
+    String id;
+    String title;
+    String description;
+    String priority;
+    String deadline;
+    String status;
+    String complete;
 
-import javax.xml.soap.Node;
-import java.time.LocalDateTime;
+    public String getId() { return id; }
 
-public class Task implements Operations{
-    Node task;
-    Memory memory = new Memory();
-    Reader openFile = new Reader();
-    Writer file = new Writer();
-    Document doc = openFile.getDoc();
+    public void setId(String id) { this.id = id; }
 
-
-    public Task(){}
-
-    public void set(Node task){
-        this.task = task;
-    }
-    public Node get(){
-        return this.task;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    @Override
-    public void view() {
-
-        NodeList taskList = memory.get();
-        System.out.println(taskList.getLength());
-        for (int je = 0; je < taskList.getLength(); je++) {
-            org.w3c.dom.Node fstNode = taskList.item(je);
-            if (fstNode.getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
-                Element des = (Element) fstNode;
-
-                NodeList listDes = des.getElementsByTagName("Description");
-                Element elListDes = (Element) listDes.item(0);
-                NodeList nodeElListDes = elListDes.getChildNodes();
-
-                NodeList listPrior = des.getElementsByTagName("Priority");
-                Element elListPrior = (Element) listPrior.item(0);
-                NodeList nodeElListPrior = elListPrior.getChildNodes();
-
-                NodeList listDLine = des.getElementsByTagName("Deadline");
-                Element elListDLine = (Element) listDLine.item(0);
-                NodeList nodeElListDLine = elListDLine.getChildNodes();
-
-                NodeList listStatus = des.getElementsByTagName("Status");
-                Element elListStatus = (Element) listStatus.item(0);
-                NodeList nodeElListStatus = elListStatus.getChildNodes();
-
-                NodeList listComp = des.getElementsByTagName("Complete");
-                Element elListComp = (Element) listComp.item(0);
-                NodeList nodeElListComp = elListComp.getChildNodes();
-                System.out.println(
-                        "Task " + taskList.item(je).getAttributes().getNamedItem("id").getNodeValue()
-                                + " ("
-                                + taskList.item(je).getAttributes().getNamedItem("caption").getNodeValue()
-                                + ") : "
-                                + ((org.w3c.dom.Node) nodeElListDes.item(0)).getNodeValue() + ", "
-                                + ((org.w3c.dom.Node) nodeElListPrior.item(0)).getNodeValue() + ", "
-                                + ((org.w3c.dom.Node) nodeElListDLine.item(0)).getNodeValue() + ", "
-                                + ((org.w3c.dom.Node) nodeElListStatus.item(0)).getNodeValue() + ", "
-                                + ((org.w3c.dom.Node) nodeElListComp.item(0)).getNodeValue()
-                );
-
-            }
-        }
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    @Override
-    public void complete(int i) {
-
-
-        Element task = memory.getTask(i);
-        // Element lang = (Element) taskList.item(i);
-        org.w3c.dom.Node status = task.getElementsByTagName("Status").item(0).getFirstChild();
-        status.setNodeValue("complete");
-        org.w3c.dom.Node complete = task.getElementsByTagName("Complete").item(0).getFirstChild();
-        complete.setNodeValue(LocalDateTime.now().toLocalDate().toString());
-        //System.out.println(((Element) .item(i)).getElementsByTagName("Status").item(0).getTextContent());
-        memory.setTask(task, i);
-
-    }
-    public void write() {
-        NodeList taskList = memory.get();
-        file.write(taskList);
+    public void setPriority(String priority) {
+        this.priority = priority;
     }
 
-    @Override
-    public void edit(int i, String title, String description, String priority, String deadline) {
-        Element taskEdit = (Element) memory.getTask(i);
-
-        taskEdit.setAttribute("caption", title);
-        if(!description.equals("-")) {
-            Element newDescriprion = (Element) taskEdit.getElementsByTagName("Description").item(0);
-            newDescriprion.setTextContent(description);
-        }
-        if(!priority.equals("-")) {
-            Element newPriority = (Element) taskEdit.getElementsByTagName("Priority").item(0);
-            newPriority.setTextContent(priority);
-        }
-        if(!deadline.equals("-")) {
-            Element newDeadline = (Element) taskEdit.getElementsByTagName("Deadline").item(0);
-            newDeadline.setTextContent(deadline);
-        }
-        System.out.println("Изменения сохранены");
-
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
     }
 
-    @Override
-    public void add(String title, String description, String priority, String deadline) {
-
-        NodeList taskList = memory.get();
-        NodeList toDoList = doc.getElementsByTagName("ToDoList");
-        Element task = (Element) toDoList.item(0);
-        Element newTask = (Element) doc.createElement("Task");
-
-        ((Element) newTask).setAttribute("caption", title);
-
-
-        Element newDescriprion = newDescriprion = (Element) doc.createElement("Description");
-        newDescriprion.setTextContent(description);
-        Element newPriority = (Element) doc.createElement("Priority");
-        newPriority.setTextContent(priority);
-        Element newDeadline = (Element) doc.createElement("Deadline");
-        newDeadline.setTextContent(deadline);
-        Element newStatus = (Element) doc.createElement("Status");
-        newStatus.setTextContent("new");
-        Element newComplete = (Element) doc.createElement("Complete");
-        newComplete.setTextContent("-");
-
-        newTask.appendChild(newDescriprion);
-        newTask.appendChild(newPriority);
-        newTask.appendChild(newDeadline);
-        newTask.appendChild(newStatus);
-        newTask.appendChild(newComplete);
-
-        NodeList list = doc.getElementsByTagName("Task");
-        int index = Integer.parseInt(list.item(list.getLength() - 1).getAttributes().getNamedItem("id").getNodeValue());
-        ((Element) newTask).setAttribute("id", String.valueOf(index + 1));
-
-        task.appendChild(newTask);
-        taskList = (NodeList) task;
-        memory.set(taskList);
-
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    @Override
-    public void remove(int i) {
-        NodeList toDoList = doc.getElementsByTagName("ToDoList");
-        NodeList taskList = memory.get();
-        Node removeNode = (Node) memory.getTask(i);
-
-        //toDoList.item(0).removeChild(taskList.item(i));
-        try {
-            toDoList.item(0).removeChild(removeNode);
-        }
-        catch(Exception ei){System.out.println("error");}
-
-        memory.set(toDoList.item(0).getChildNodes());
-
-
+    public void setComplete(String complete) {
+        this.complete = complete;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getComplete() {
+        return complete;
+    }
+
+
+
+
 }
